fcd6aebc69a19ed5d16e61d0416518f9ffa71797
alban-pierre <alban.pierre@ens.fr>
2017-01-04 18:39:24 +0100

    Update optical_flow

--- optical_flow.m
+++ optical_flow.m
@@ -1,7 +1,6 @@
-function opf = optical_flow(n_vid, frames)
+function opf = optical_flow(n_vid, frames, alpha)
 
     maxIter = 10;
-    alpha = 3;
     
     if (n_vid < 10)
         dr = sprintf('ArrowDataAll/00%d/', n_vid);
@@ -35,6 +34,9 @@ function opf = optical_flow(n_vid, frames)
     im1 = im1/2;
     im2 = im2/2;
     
+    opf = zeros(size(im1,1)-2, size(im1,2)-2,3, size(frames,2), 'int16');
+    iopf = 1;
+    
     for f = frames
         if (f+1 < 10)
             filepathf = sprintf('%sim0000000%d.jpeg', dr, f+1);
@@ -53,34 +55,34 @@ function opf = optical_flow(n_vid, frames)
         fy = int8(im3(3:end,2:end-1,:)) - int8(im1(1:end-2,2:end-1,:));
         ft = int8(im3(2:end-1,2:end-1,:)) - int8(im1(2:end-1,2:end-1,:));
         
-        u = zeros(size(im1,1)-2, size(im1,2)-2,3, 'int16');
-        v = zeros(size(im1,1)-2, size(im1,2)-2,3, 'int16');
-        uav = zeros(size(im1,1)-2, size(im1,2)-2, 3, 'int16');
-        vav = zeros(size(im1,1)-2, size(im1,2)-2, 3, 'int16');
+        u = zeros(size(im1,1)-2, size(im1,2)-2,3, 'int8');
+        v = zeros(size(im1,1)-2, size(im1,2)-2,3, 'int8');
+        uav = zeros(size(im1,1)-2, size(im1,2)-2, 3, 'int8');
+        vav = zeros(size(im1,1)-2, size(im1,2)-2, 3, 'int8');
 
         D = int16(alpha^2 + fx.^2 + fy.^2);
-        u = int16(-int16(fx.*ft)./D);
-        v = int16(-int16(fy.*ft)./D);
+        u = int8(-int16(fx.*ft)./D);
+        v = int8(-int16(fy.*ft)./D);
         
         for it=1:maxIter
-            uav = zeros(size(im1,1)-2, size(im1,2)-2, 3, 'int16');
+            uav = zeros(size(im1,1)-2, size(im1,2)-2, 3, 'int8');
             uav(:,1:end-1,:) += (u(:,2:end,:))/4;
             uav(:,2:end,:) += (u(:,1:end-1,:))/4;
             uav(1:end-1,:,:) += (u(2:end,:,:))/4;
             uav(2:end,:,:) += (u(1:end-1,:,:))/4;
-            vav = zeros(size(im1,1)-2, size(im1,2)-2, 3, 'int16');
+            vav = zeros(size(im1,1)-2, size(im1,2)-2, 3, 'int8');
             vav(:,1:end-1,:) += (v(:,2:end,:))/4;
             vav(:,2:end,:) += (v(:,1:end-1,:))/4;
             vav(1:end-1,:,:) += (v(2:end,:,:))/4;
             vav(2:end,:,:) += (v(1:end-1,:,:))/4;
 
-            D = int16(alpha^2 + fx.^2 + fy.^2);
-            u = int16(-int16(fx.*ft)./D);
-            v = int16(-int16(fy.*ft)./D);
+            P = int8(fx.*uav + fy.*vav + ft);
+            u = int8(-int16(fx.*P)./D);
+            v = int8(-int16(fy.*P)./D);
         end
 
-
-
+        opf(:,:,:,iopf) = int16(u.^2 + v.^2);
+        iopf++;
 
         im1 = im2;
         im2 = im3;
